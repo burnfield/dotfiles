86a2b9c5e944c9006b21cbab755aad235dbff421
Oskar Branfeldt <oskar.branfeldt@gmail.com>
2024-06-25 08:44:03 +0200

    nixos: various misc changes

--- nixos/oskar.nix
+++ nixos/oskar.nix
@@ -7,50 +7,54 @@
 {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowBroken = true;
 
-  fonts.packages = with pkgs; [
-    nerdfonts
-  ];
+  fonts.packages = with pkgs; [ nerdfonts ];
 
   services.xserver = {
     enable = true;
     xkb.layout = "us,se";
     xkb.variant = "";
     xkb.options = "ctrl:nocaps,grp:lctrl_lshift_toggle";
-    desktopManager = {
-      xterm.enable = false;
-    };
+    desktopManager = { xterm.enable = false; };
 
     windowManager.i3 = {
       enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status-rust
-        i3blocks
-      ];
+      extraPackages = with pkgs; [ dmenu i3status-rust i3blocks ];
     };
   };
 
   services.displayManager.defaultSession = "none+i3";
 
-  programs.firefox = {
-    enable = true;
-    nativeMessagingHosts.packages = [ pkgs.plasma5Packages.plasma-browser-integration ];
+  programs = {
+    firefox = {
+      enable = true;
+      nativeMessagingHosts.packages =
+        [ pkgs.plasma5Packages.plasma-browser-integration ];
+    };
+    direnv = {
+      enable = true;
+      silent = false;
+      loadInNixShell = true;
+      nix-direnv = {
+        enable = true;
+        package = pkgs.nix-direnv;
+      };
+    };
   };
-
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     alacritty
-    # cargo
     clang
+    clang-tools
     cura # 3d printer
     fd
     feh
+    man-pages
+    man-pages-posix
     font-awesome
-    firefox-bin
-    flashprint # flashforge printer goto10
     chromium
     fzf
     git
@@ -59,7 +63,6 @@
     luajitPackages.luarocks
     lua-language-server
     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-#    nodejs_21
     powerline-go
     python3
     python311Packages.pip
@@ -67,8 +70,6 @@
     python311Packages.venvShellHook
     ripgrep
     rustup
-    # rustc
-    rust-analyzer
     stow
     tig
     tmux
