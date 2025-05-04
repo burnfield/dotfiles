7a689956779609de0aa2c198e65f505283f849a0
Oskar Branfeldt <oskar.branfeldt@gmail.com>
2025-05-04 21:29:22 +0200

    WIP

--- home/wezterm/.config/wezterm/wezterm.lua
+++ home/wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,22 @@
-local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'Batman'
+config.color_scheme = "catppuccin-latte"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.window_frame = {
+  font_size = 10,
+}
+
+-- domains/workspaces
+config.unix_domains = {
+  {
+    name = "unix",
+  },
+}
+config.default_gui_startup_args = { "connect", "unix" }
 
 return config
