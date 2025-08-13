003bc2f3114b79a62cf4ad5ad2a93a11e4b3f6a8
Oskar Bränfeldt <oskar.branfeldt@gmail.com>
2025-08-13 09:59:46 +0200

    wezterm update

--- home/wezterm/.config/wezterm/wezterm.lua
+++ home/wezterm/.config/wezterm/wezterm.lua
@@ -1,22 +1,17 @@
-local config = {}
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
 
 config.color_scheme = "catppuccin-latte"
+config.font = wezterm.font_with_fallback {
+  'Comic Code',
+}
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
-config.window_frame = {
-  font_size = 10,
-}
-
--- domains/workspaces
-config.unix_domains = {
-  {
-    name = "unix",
-  },
-}
-config.default_gui_startup_args = { "connect", "unix" }
+config.window_decorations = "NONE"
+config.warn_about_missing_glyphs = false
 
 return config
